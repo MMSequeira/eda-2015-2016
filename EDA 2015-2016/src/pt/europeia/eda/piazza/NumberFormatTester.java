be53c70fc90a85da10815a389185d9c369de2f00
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2016-04-05 17:13:27 +0100

    Correct comments and improve demonstration of roundings.

--- EDA 2015-2016/src/pt/europeia/eda/piazza/NumberFormatTester.java
+++ EDA 2015-2016/src/pt/europeia/eda/piazza/NumberFormatTester.java
@@ -13,77 +13,51 @@ public class NumberFormatTester {
     // against directly creating instances, instead of using NumberFormat
     // factory methods.
     public static void main(final String[] arguments) {
-        // This pattern leads to numbers being formatted with two decimal
-        // places, using rounding (DecimalFormat uses "half even" rounding by
-        // default):
-        DecimalFormat formatter = new DecimalFormat("0.00");
+        // DecimalFormat uses "half even" rounding by default. That is, is case
+        // of a tie, the number if rounded so that the last digit is even.
 
-        // Since 4.445 is equally close to 4.44 and to 4.45, it is rounded so
-        // the next digit is even (down, in this case). Hence, the expected
-        // result is "4.44":
-        out.println(formatter.format(4.445) + " (4.445 → 4.44 expected)");
+        out.println("Rounding to two decimal places:");
 
-        // Since 4.446 is closer to 4.45 than to 4.44, it is rounded up. Hence,
-        // the expected result is "4.45":
-        out.println(formatter.format(4.446) + " (4.446 → 4.45 expected)");
+        final DecimalFormat f2 = new DecimalFormat("0.00");
 
-        // Since 4.4445 is closer to 4.44 than to 4.45, it is rounded down.
-        // Hence, the expected result is "4.44":
-        out.println(formatter.format(4.4445) + " (4.4445 → 4.44 expected)");
+        out.println("\t" + f2.format(4.125) + " (4.125 → 4.12 got)");
+        out.println("\t" + f2.format(4.375) + " (4.375 → 4.38 got)");
+        out.println("\t" + f2.format(4.445) + " (4.445 → 4.44 expected)");
+        out.println("\t" + f2.format(4.446) + " (4.446 → 4.45 got)");
+        out.println("\t" + f2.format(4.4445) + " (4.4445 → 4.44 got)");
+        out.println("\t" + f2.format(4.4446) + " (4.4446 → 4.44 got)");
 
-        // Since 4.4446 is closer to 4.44 than to 4.45, it is rounded down.
-        // Hence, the expected result is "4.44":
-        out.println(formatter.format(4.4446) + " (4.4446 → 4.44 expected)");
+        out.println("Rounding to three decimal places:");
 
-        // Since 4.4415 is equally close to 4.441 and to 4.442, it is rounded so
-        // the next digit is even (up, in this case). Hence, the expected result
-        // is "4.442":
-        out.println(new DecimalFormat("0.000").format(4.4415)
-                + " (4.4415 → 4.442 expected)");
+        final DecimalFormat f3 = new DecimalFormat("0.000");
 
-        // Since 4.4416 is closer to 4.442 than to 4.441, it is rounded up.
-        // Hence, the expected result is "4.442":
-        out.println(new DecimalFormat("0.000").format(4.4416)
-                + " (4.4416 → 4.442 expected)");
+        out.println("\t" + f3.format(4.4415) + " (4.4415 → 4.442 expected)");
+        out.println("\t" + f3.format(4.4416) + " (4.4416 → 4.442 got)");
+        out.println("\t" + f3.format(4.4445) + " (4.4445 → 4.444 got)");
+        out.println("\t" + f3.format(4.4446) + " (4.4446 → 4.445 got)");
 
-        // Since 4.4445 is equally close to 4.444 and to 4.445, it is
-        // rounded so the next digit is even (down, in this case). Hence, the
-        // expected result is "4.444":
-        out.println(new DecimalFormat("0.000").format(4.4445)
-                + " (4.4445 → 4.444 expected)");
+        out.println("Numbers with ties revealed:");
 
-        // Since 4.4446 is closer to 4.445 and to 4.444, it is rounded up.
-        // Hence, the expected result is "4.445":
-        out.println(new DecimalFormat("0.000").format(4.4446)
-                + " (4.4446 → 4.445 expected)");
+        // A hint to the unexpected results in case of apparent ties is given by
+        // running the code below. As can be seen, the literal 4.445 is actually
+        // stored as a a larger number. On the other hand, the the literals
+        // 4.4415 and 4.4445 are actually stored as smaller numbers and hence,
+        // when rounded to three decimal places, they are actually rounded down.
+        // That is, these ties are really apparent only:
+        out.println("\t4.445 as BigDecimal is " + new BigDecimal(4.445));
+        out.println("\t4.4415 as BigDecimal is " + new BigDecimal(4.4415));
+        out.println("\t4.4445 as BigDecimal is " + new BigDecimal(4.4445));
 
-        // The truth is revealed by running the code below. As
-        // can be seen, the literal 4.445 is actually stored as a a larger
-        // number. On the other hand, the the literals 4.4415 and 4.4445 are
-        // actually stored as smaller numbers and hence, when rounded to three
-        // decimal places, they are actually rounded down:
-        out.println(new BigDecimal(4.445));
-        out.println(new BigDecimal(4.4415));
-        out.println(new BigDecimal(4.4445));
+        // The other ties occur with numbers that are represented exactly, and
+        // hence rounding occurs as expected. These are real ties:
+        out.println("\t4.125 as BigDecimal is " + new BigDecimal(4.125));
+        out.println("\t4.375 as BigDecimal is " + new BigDecimal(4.375));
 
-        // These numbers are represented exactly, and hence rounding occurs as
-        // expected:
-        out.println(new DecimalFormat("0.00").format(4.125));
-        out.println(new DecimalFormat("0.00").format(4.375));
-        out.printf("%.2f\n", 4.125);
-        out.printf("%.2f\n", 4.375);
-        out.println(new BigDecimal(4.125));
-        out.println(new BigDecimal(4.375));
-
-        // The initial case seemed to work well only because the actual number
-        // store was larger, and hence it was rounded up, as expected:
-        // out.println(new BigDecimal(4.445));
-
-        // Please remember: floats and doubles are store using a base 2
+        // Please remember: floats and doubles are stored using a base 2
         // representation. Numbers with a finite number of digits after the
         // point in base 10 can actually have an infinite number of digits after
         // the point in base 2. Since floats and double store only a finite
-        // number of digits most decimal literals are approximated, rather than
+        // number of digits, most decimal literals are approximated, rather than
         // represented exactly.
     }
 
