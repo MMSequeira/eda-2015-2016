82c872544475aef3843b6dd652ba24e5021bea06
Manuel Menezes de Sequeira <mmsequeira@gmail.com>
2016-04-22 16:41:08 +0100

    Add files for IG-B session 14.

---
+++ EDA 2015-2016/src/pt/europeia/eda/igb/session14/DefaultDoubleArrayValuesTest.java
@@ -0,0 +1,16 @@
+package pt.europeia.eda.igb.session14;
+
+import static java.lang.System.out;
+
+public class DefaultDoubleArrayValuesTest {
+
+    // Simple example of default item values for double and Double arrays:
+    public static void main(final String[] arguments) {
+        final Double[] values1 = new Double[1];
+        final double[] values2 = new double[1];
+
+        out.println(values1[0]);
+        out.println(values2[0]);
+    }
+
+}
